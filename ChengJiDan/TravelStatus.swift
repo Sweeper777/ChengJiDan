5804f2b77ff75624d26fd9099ad1a203380ab3f9
Mulang Su <sumulang@gmail.com>
2020-03-17 20:00:25 +0000

    Create TravelStatus.swift

---
+++ ChengJiDan/TravelStatus.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum TravelStatus: Int, CustomDebugStringConvertible {
+    case untrodden
+    case passedThrough
+    case landed
+    case visited
+    case spentTheNight
+    case lived
+    
+}
