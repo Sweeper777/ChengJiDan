ab9a0f5090cff2c20de5abc48167d9abcfc1dd00
Mulang Su <sumulang@gmail.com>
2020-03-17 20:00:43 +0000

    added TravelStatus.debugDescription

--- ChengJiDan/TravelStatus.swift
+++ ChengJiDan/TravelStatus.swift
@@ -8,4 +8,20 @@ enum TravelStatus: Int, CustomDebugStringConvertible {
     case spentTheNight
     case lived
     
+    var debugDescription: String {
+        switch self {
+        case .untrodden:
+            return "untrodden"
+        case .passedThrough:
+            return "passedThrough"
+        case .landed:
+            return "landed"
+        case .visited:
+            return "visited"
+        case .spentTheNight:
+            return "spentTheNight"
+        case .lived:
+            return "lived"
+        }
+    }
 }
