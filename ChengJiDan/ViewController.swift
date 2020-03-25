c25da51b1b9fc7b895064f73f3de152f1175f2bf
Mulang Su <sumulang@gmail.com>
2020-03-25 13:37:53 +0000

    added addChengJiDanTapped

--- ChengJiDan/ViewController.swift
+++ ChengJiDan/ViewController.swift
@@ -23,5 +23,19 @@ class ChengJiDanListViewController: UITableViewController {
         cell.textLabel?.text = chengJiDans[indexPath.row].name
         return cell
     }
+    
+    @IBAction func addChengJiDanTapped() {
+        let chengJiDansWithDefaultNames = DataManager.shared.queryChengJiDan("name BEGINSWITH %@", args: "城跡单")
+        let nextNumber = (chengJiDansWithDefaultNames.compactMap { Int($0.name.dropFirst(3)) }.max() ?? 0) + 1
+        
+        let alert = SCLAlertView()
+        let textField = alert.addTextField("给新城跡单取个名字吧！")
+        textField.text = "城跡单\(nextNumber)"
+        alert.addButton("确定") {
+            self.addChengJiDan(name: textField.text ?? "")
+        }
+        alert.showEdit("新城跡单", subTitle: nil, closeButtonTitle: "取消")
+    }
+    
 }
 
