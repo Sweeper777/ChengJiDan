944298d9cda02fc01bc500b9c495a50ad856bd8b
Mulang Su <sumulang@gmail.com>
2020-03-25 13:37:41 +0000

    added table view methods

--- ChengJiDan/ViewController.swift
+++ ChengJiDan/ViewController.swift
@@ -10,6 +10,18 @@ class ChengJiDanListViewController: UITableViewController {
         chengJiDans = DataManager.shared.allChengJiDan
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        chengJiDans.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = chengJiDans[indexPath.row].name
+        return cell
+    }
 }
 
