6803fdbf7c60faa42bc1ae63e0917f9a8576eadc
Mulang Su <sumulang@gmail.com>
2020-03-25 13:38:23 +0000

    Create ChengJiDanMapViewController.swift

---
+++ ChengJiDan/ChengJiDanMapViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SwiftyXMLParser
+
+class ChengJiDanMapViewController : UIViewController {
+    var chengJiDan: ChengJiDanMap!
+    
+    @IBOutlet var svgView: SVGView!
+    
+    override func viewDidLoad() {
+    }
+    
+}
