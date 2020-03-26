21b9a976ce3d374c9f489ac074a25ce2c070fd4d
Mulang Su <sumulang@gmail.com>
2020-03-26 20:55:09 +0000

    added table view methods

--- ChengJiDan/ChengJiDanEditorViewController.swift
+++ ChengJiDan/ChengJiDanEditorViewController.swift
@@ -11,5 +11,24 @@ class ChengJiDanEditorViewController : UITableViewController {
         dataSource = Province.allCases
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        dataSource.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataSource[section].cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        dataSource[section].name
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = dataSource[indexPath.section].cities[indexPath.row]
+        let status = cityStatusPairDict[dataSource[indexPath.section].cities[indexPath.row]] ?? .untrodden
+        cell.detailTextLabel?.text = status.description
+        return cell
+    }
 }
 
