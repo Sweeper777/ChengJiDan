a1f57b23b790805427012f2fbc1446beb425124c
Mulang Su <sumulang@gmail.com>
2020-03-26 20:53:16 +0000

    added ChengJiDanMap.totalForEachProvince

--- ChengJiDan/ChengJiDanMapViewController.swift
+++ ChengJiDan/ChengJiDanMapViewController.swift
@@ -10,3 +10,12 @@ class ChengJiDanMapViewController : UIViewController {
     }
     
 }
+
+extension ChengJiDanMap {
+    var totalForEachProvince: [Province: UIColor] {
+        let groups = Dictionary(grouping: entries, by: { Province(city: $0.city) })
+        var totals = groups.mapValues { $0.map { $0.status.rawValue }.max() ?? 0 }
+        totals[nil] = nil
+        return totals.mapValues { UIColor(named: TravelStatus(rawValue: $0)!.debugDescription) } as! [Province: UIColor]
+    }
+}
