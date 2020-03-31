d2f84c28e7ff91db61a3ebd028253af765927bca
Mulang Su <sumulang@gmail.com>
2020-03-31 08:57:32 +0100

    removed dummy chengJiDan

--- ChengJiDan/ChengJiDanMapViewController.swift
+++ ChengJiDan/ChengJiDanMapViewController.swift
@@ -7,19 +7,6 @@ class ChengJiDanMapViewController : UIViewController {
     @IBOutlet var svgView: SVGView!
     
     override func viewDidLoad() {
-        chengJiDan = ChengJiDanMap(name: "", entries: [
-            CityStatusPair(city: "沈阳市", status: .lived),
-            CityStatusPair(city: "北京市", status: .lived),
-            CityStatusPair(city: "香港特别行政区", status: .lived),
-            CityStatusPair(city: "承德市", status: .spentTheNight),
-            CityStatusPair(city: "上海市", status: .spentTheNight),
-            CityStatusPair(city: "广州市", status: .spentTheNight),
-            CityStatusPair(city: "石家庄市", status: .passedThrough),
-            CityStatusPair(city: "青岛市", status: .passedThrough),
-            CityStatusPair(city: "天津市", status: .landed),
-            CityStatusPair(city: "武汉市", status: .visited),
-        ])
-        
         let xmlString = try! String(contentsOfFile: Bundle.main.path(forResource: "map", ofType: "svg")!)
         let xml = try! XML.parse(xmlString)
         svgView.svgStrings = Array(xml["svg", "g" ,"path"])
