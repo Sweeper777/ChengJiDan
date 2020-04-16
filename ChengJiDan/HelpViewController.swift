af84e06a907384e08f062a69f701a3f192941215
Mulang Su <sumulang@gmail.com>
2020-04-16 11:10:27 +0100

    read help text from file

--- ChengJiDan/HelpViewController.swift
+++ ChengJiDan/HelpViewController.swift
@@ -1,6 +1,18 @@
 import UIKit
+import DCTextEngine
 
 class HelpViewController: UIViewController {
+    @IBOutlet var helpTextView: UITextView!
+    
+    override func viewDidLoad() {
+        let engine = DCTextEngine.withMarkdown()!
+        guard let markdownString = try? String(contentsOfFile: Bundle.main.path(forResource: "help", ofType: "md")!) else {
+            return
+        }
+        let attributedString = engine.parse(markdownString)
+        helpTextView.attributedText = attributedString
+    }
+    
     @IBAction func doneTapped() {
         dismiss(animated: true, completion: nil)
     }
