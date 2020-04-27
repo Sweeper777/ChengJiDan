1d05396de7ebea50fbc73441e95cc18c97ac7f7e
Mulang Su <sumulang@gmail.com>
2020-04-27 11:08:44 +0100

    set up GAD

--- ChengJiDan/AppDelegate.swift
+++ ChengJiDan/AppDelegate.swift
@@ -8,6 +8,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [
+            kGADSimulatorID as! String
+        ]
         return true
     }
 
