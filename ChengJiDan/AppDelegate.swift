af06bcf3bce9f52bdcbac6746a48a7766328e069
Mulang Su <sumulang@gmail.com>
2020-04-27 11:08:36 +0100

    set up firebase

--- ChengJiDan/AppDelegate.swift
+++ ChengJiDan/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  ChengJiDan
-//
-//  Created by Mulang Su on 14/03/2020.
-//  Copyright © 2020 Mulang Su. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
