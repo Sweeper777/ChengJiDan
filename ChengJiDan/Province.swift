a3c0fbec81e0ca947b9303881cbfedb0bb41224f
Mulang Su <sumulang@gmail.com>
2020-05-16 10:58:06 +0100

    made Province Codable and added abbreviation property

--- ChengJiDan/Province.swift
+++ ChengJiDan/Province.swift
@@ -1,17 +1,19 @@
 import RealmSwift
 import Realm
 
-struct Province {
+struct Province : Codable {
     let name: String
     let shortName: String
     let svgPathIndex: Int
     let cities: [String]
+    let abbreviation: String
     
     init(name: String, shortName: String, svgPathIndex: Int, cities: [String]) {
         self.name = name
         self.shortName = shortName
         self.svgPathIndex = svgPathIndex
         self.cities = cities
+        self.abbreviation = ""
     }
     
     init?(city: String) {
