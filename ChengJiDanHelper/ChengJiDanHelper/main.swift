5407479f6065bb30444cdf743ee1f2fb12db7870
Mulang Su <sumulang@gmail.com>
2020-07-16 08:08:09 +0800

    added loadMap

--- ChengJiDanHelper/ChengJiDanHelper/main.swift
+++ ChengJiDanHelper/ChengJiDanHelper/main.swift
@@ -1,2 +1,11 @@
 import Foundation
 
+func loadMap(fromAdcode adcode: String, includeSubregions: Bool) throws -> FeatureCollection {
+    let data = try Data(contentsOf:URL(string:
+        "https://geo.datav.aliyun.com/areas_v2/bound/\(adcode)\(includeSubregions ? "_full" : "").json")!)
+    let decoder = JSONDecoder()
+    let featureCollection = try decoder.decode(FeatureCollection.self, from: data)
+    try data.write(to: URL(fileURLWithPath: "/Users/mulangsu/Desktop/geojson maps/\(adcode).geojson"))
+    return featureCollection
+}
+
