0cf06e571a1cd4f37e78885ed2e1f9e702ad0e5a
Mulang Su <sumulang@gmail.com>
2020-07-16 08:08:21 +0800

    downloaded all submaps and combined

--- ChengJiDanHelper/ChengJiDanHelper/main.swift
+++ ChengJiDanHelper/ChengJiDanHelper/main.swift
@@ -9,3 +9,21 @@ func loadMap(fromAdcode adcode: String, includeSubregions: Bool) throws -> Featu
     return featureCollection
 }
 
+print("Downloading whole country map...")
+let wholeCountry = try loadMap(fromAdcode: "100000", includeSubregions: true)
+var subMaps = [FeatureCollection]()
+for province in wholeCountry.features {
+    guard !province.properties.name.isEmpty else { continue }
+    let includeSubregions = !["北京市", "重庆市", "天津市", "上海市", "香港", "澳门", "台湾省"]
+        .contains(province.properties.name)
+    print("Downloading map of \(province.properties.name)...")
+    subMaps.append(try loadMap(fromAdcode: province.properties.adcode, includeSubregions: includeSubregions))
+    
+}
+print("Downloaded all the submaps. Combining...")
+let allFeatures = subMaps.map(\.features).reduce([], +)
+let bigFeatureCollection = FeatureCollection(type: "FeatureCollection", features: allFeatures)
+print("All combined! Writing data...")
+let encoder = JSONEncoder()
+let data = try encoder.encode(bigFeatureCollection)
+try data.write(to: URL(fileURLWithPath: "/Users/mulangsu/Desktop/geojson maps/city level map.geojson"))
