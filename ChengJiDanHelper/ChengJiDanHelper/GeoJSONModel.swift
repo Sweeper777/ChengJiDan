373f9642007db27a46f45e4695e38b7aab43ed8e
Mulang Su <sumulang@gmail.com>
2020-07-16 08:07:55 +0800

    implemented custom decoding

--- ChengJiDanHelper/ChengJiDanHelper/GeoJSONModel.swift
+++ ChengJiDanHelper/ChengJiDanHelper/GeoJSONModel.swift
@@ -30,6 +30,35 @@ struct Properties: Codable {
     let parent: Parent?
     let subFeatureIndex: Int?
     let acroutes: [Int]?
+    
+    enum CodingKeys : CodingKey {
+        case adcode
+        case name
+        case center
+        case centroid
+        case childrenNum
+        case level
+        case parent
+        case subFeatureIndex
+        case acroutes
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        do {
+            adcode = try container.decode(String.self, forKey: .adcode)
+        } catch {
+            adcode = try container.decode(Int.self, forKey: .adcode).description
+        }
+        name = try container.decode(String.self, forKey: .name)
+        center = try container.decodeIfPresent([Double].self, forKey: .center)
+        centroid = try container.decodeIfPresent([Double].self, forKey: .centroid)
+        childrenNum = try container.decodeIfPresent(Int.self, forKey: .childrenNum)
+        level = try container.decodeIfPresent(String.self, forKey: .level)
+        parent = try container.decodeIfPresent(Parent.self, forKey: .parent)
+        acroutes = try container.decodeIfPresent([Int].self, forKey: .acroutes)
+        subFeatureIndex = try container.decodeIfPresent(Int.self, forKey: .subFeatureIndex)
+    }
 }
 
 // MARK: - Parent
