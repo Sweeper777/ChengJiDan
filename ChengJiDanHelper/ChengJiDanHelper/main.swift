af3841bab532d6c2ea5d2c8361852f23091c715f
Mulang Su <sumulang@gmail.com>
2020-07-18 17:45:27 +0800

    Oops! typo

--- ChengJiDanHelper/ChengJiDanHelper/main.swift
+++ ChengJiDanHelper/ChengJiDanHelper/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func combineMaps() {
+func combineMaps() throws{
     func loadMap(fromAdcode adcode: String, includeSubregions: Bool) throws -> FeatureCollection {
         let data = try Data(contentsOf:URL(string:
             "https://geo.datav.aliyun.com/areas_v2/bound/\(adcode)\(includeSubregions ? "_full" : "").json")!)
