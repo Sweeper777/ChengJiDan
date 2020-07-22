883e18289c2703e478db75ef7a41ac859e7f7adb
Mulang Su <sumulang@gmail.com>
2020-07-22 20:06:29 +0800

    added GeoJSONMap.draw

--- ChengJiDan/GeoJSONMapView.swift
+++ ChengJiDan/GeoJSONMapView.swift
@@ -8,6 +8,29 @@ class GeoJSONMapView : UIView {
         }
     }
     
+    override func draw(_ rect: CGRect) {
+        UIColor.label.setStroke()
+        guard let featureCollection = self.featureCollection else { return }
+        let features = featureCollection.features
+        for feature in features {
+            print("Drawing \(feature.properties!.name)")
+            guard let multipolygon = feature.geometry?.coordinates else { continue }
+            let multiPolygonPath = UIBezierPath()
+            for polygon in multipolygon {
+                let firstLinearRing = polygon.first!
+                for (index, position) in firstLinearRing.enumerated() {
+                    if index == 0 {
+                        multiPolygonPath.move(to: project(long: position.longitude, lat: position.latitude))
+                    } else {
+                        multiPolygonPath.addLine(to: project(long: position.longitude, lat: position.latitude))
+                    }
+                }
+                multiPolygonPath.close()
+            }
+            multiPolygonPath.lineWidth = 0.3
+            multiPolygonPath.stroke()
+        }
+    }
     
     func project(long: Double, lat: Double) -> CGPoint {
         let projectedLong = ((long - 73.5) / 61.25).f * width
