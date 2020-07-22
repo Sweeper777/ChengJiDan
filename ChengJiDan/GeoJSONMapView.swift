5f70a4dbc18e21991710a27346fca55a53204183
Mulang Su <sumulang@gmail.com>
2020-07-22 20:05:52 +0800

    added projection function

--- ChengJiDan/GeoJSONMapView.swift
+++ ChengJiDan/GeoJSONMapView.swift
@@ -8,4 +8,10 @@ class GeoJSONMapView : UIView {
         }
     }
     
+    
+    func project(long: Double, lat: Double) -> CGPoint {
+        let projectedLong = ((long - 73.5) / 61.25).f * width
+        let projectedLat = (1 - ((lat - 8.8) / 44.8).f) * height
+        return CGPoint(x: projectedLong, y: projectedLat)
+    }
 }
