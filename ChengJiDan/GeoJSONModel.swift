177e089dcd25b548c9f59bbece8b47b8db3d0693
Mulang Su <sumulang@gmail.com>
2020-07-23 10:50:26 +0800

    added GeoJSONManager

--- ChengJiDan/GeoJSONModel.swift
+++ ChengJiDan/GeoJSONModel.swift
@@ -5,3 +5,25 @@ struct ProvinceProperties: Codable {
 }
 
 typealias MapFeatureCollection = GeoJSONFeatureCollection<MultiPolygonGeometry, ProvinceProperties>
+
+class GeoJSONManager {
+    private static var china: MapFeatureCollection?
+    private static let geoJSONLoaderQueue = DispatchQueue(label: "geoJSONLoader", qos: .background)
+    
+    private init() {}
+    
+    static func loadChinaGeoJSON(completion: @escaping (MapFeatureCollection) -> Void) {
+        if let china = GeoJSONManager.china {
+            DispatchQueue.main.async { completion(china) }
+            return
+        }
+        geoJSONLoaderQueue.async {
+            let data = try! Data(contentsOf: Bundle.main.url(forResource: "city level map", withExtension: "geojson")!)
+            let featureCollection = try! JSONDecoder().decode(MapFeatureCollection.self, from: data)
+            DispatchQueue.main.async {
+                completion(featureCollection)
+                GeoJSONManager.china = featureCollection
+            }
+        }
+    }
+}
